13dffd9225a5de787529b162c4f0631d551f8eb8
Maciek <maciejsdev@gmail.com>
2024-06-08 12:17:32 +0200

    sidebar + modal added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import Modal from "./components/Modal";
+import SideBar from "./components/SideBar";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <SideBar />
+    </>
+  );
+};
+export default App;
